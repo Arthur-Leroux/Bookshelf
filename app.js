c4751367b3a1d4f1bb38388ec887faf3dfc17b7b
Arthur LEROUX <arthur.leroux33@protonmail.com>
2023-07-25 22:03:29 +0200

    feat for of books and dynamic hyperlien

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 const http = require(`http`);
 //j'importe my-modules
 const books = require(`./my-modules/books`);
+
 //console.log(books);
 //j'instancie mon server
-const  server = http.createServer();
+const server = http.createServer();
+const localhost = "http://localhost:";
+const lienBook = '/books';
 //j'écoute les requêtes
-server.on(`request`,(req,res)=>{
+server.on(`request`, (req, res) => {
+  const url = req.url;
+  //je place en dur mon tableau
 
-
-//je place en dur mon tableau 
-
-
-res.write(`<!DOCTYPE html>
+  res.write(`<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -21,22 +22,37 @@ res.write(`<!DOCTYPE html>
 </head>
 <body>`);
 
+  switch (url) {
+    case `/`:
+      res.write(
+        `voici la liste des bouquins réferencé => 
+        <a href="${localhost}${port}${lienBook}">ici</a>`
+      );
+break;
+    case `/books`:
+      for (let book of books) {
+        res.write(`<ul>
+    <li>voici les livres :${book.title}
+    language : ${book.language}
+    country :${book.country}
+    author :${book.author}
+    date :${book.date}
+    </li></ul>`);
+      }
+      break;
+      default:
+        res.write(`cette page n'est pas disponible`);
+  }
+  
+  res.write(`</body>
+</html>`);
 
-
-
-
-
-res.write(`</body>
-</html>`)
-
-
-    res.end();
+  res.end();
 });
-
 
 //j'écoute le port
 const port = 5000;
 
-server.listen(port,()=>{
-    console.log(`Start on : http://localhost:${port}`);
-})
+server.listen(port, () => {
+  console.log(`Start on : ${localhost}${port}`);
+});
