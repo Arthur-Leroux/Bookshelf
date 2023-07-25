c6188c641ec4765f7fed069ad2d1be997fbfa3ae
Arthur LEROUX <arthur.leroux33@protonmail.com>
2023-07-25 21:03:58 +0200

    feat: create module books

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 //j'importe le module natif http de node
 const http = require(`http`);
-
-
+//j'importe my-modules
+const books = require(`./my-modules/books`);
+//console.log(books);
 //j'instancie mon server
 const  server = http.createServer();
 //j'écoute les requêtes
 server.on(`request`,(req,res)=>{
 
 
-res.write(`coucou`);
+//je place en dur mon tableau 
+
+
+res.write(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>`);
+
+
+
+
+
+
+res.write(`</body>
+</html>`)
 
 
     res.end();
