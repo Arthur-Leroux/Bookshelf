813e41a03bb755af50ebd8b432fa96ab6c1878bb
Arthur LEROUX <arthur.leroux33@protonmail.com>
2023-07-25 20:55:06 +0200

    feat: start on server

---
+++ app.js
@@ -0,0 +1,24 @@
+//j'importe le module natif http de node
+const http = require(`http`);
+
+
+//j'instancie mon server
+const  server = http.createServer();
+//j'écoute les requêtes
+server.on(`request`,(req,res)=>{
+
+//je place en dur mon tableau 
+
+res.write(`coucou`);
+
+
+    res.end();
+});
+
+
+//j'écoute le port
+const port = 5000;
+
+server.listen(port,()=>{
+    console.log(`Start on : http://localhost:${port}`);
+})
